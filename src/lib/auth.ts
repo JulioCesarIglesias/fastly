0977096361f4e9d6a5ee8538e6edb4c09436fe95
Júlio César <juliocesariglesias77@gmail.com>
2026-05-05 20:59:39 -0300

    feat(auth): integrate Google OAuth login with better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,11 +5,22 @@ import { db } from "@/db";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://festly.com.br",
+    // "https://*.festly.com.br",
+  ],
   database: drizzleAdapter(db, {
     provider: "pg", // or "sqlite" or "mysql"
     usePlural: true,
     schema,
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   //... the rest of your config
   user: {
     modelName: "usersTable",
